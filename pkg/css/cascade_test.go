79d9dbce4e66d53207b2848bb1fc26752479a835
Nathan Stitt <nathan@stitt.org>
2026-06-23 15:31:22 -0500

    Broaden initial ComputedStyle test coverage

--- pkg/css/cascade_test.go
+++ pkg/css/cascade_test.go
@@ -16,4 +16,24 @@ func TestInitialComputedStyle(t *testing.T) {
 	if cs.FontSizePt != 16 { // 16px default medium, expressed in px-as-pt placeholder
 		t.Fatalf("initial font-size = %v, want 16", cs.FontSizePt)
 	}
+	if cs.FontFamily != "serif" {
+		t.Errorf("initial font-family = %q, want serif", cs.FontFamily)
+	}
+	if cs.LineHeight.Unit != UnitAuto {
+		t.Errorf("initial line-height unit = %v, want UnitAuto (normal)", cs.LineHeight.Unit)
+	}
+	if cs.TextAlign != "left" {
+		t.Errorf("initial text-align = %q, want left", cs.TextAlign)
+	}
+	if cs.Width.Unit != UnitAuto || cs.Height.Unit != UnitAuto {
+		t.Errorf("initial width/height = %v/%v, want auto/auto", cs.Width.Unit, cs.Height.Unit)
+	}
+	// BackgroundColor is transparent by zero value (alpha 0 = "not set").
+	if cs.BackgroundColor != (color.RGBA{}) {
+		t.Errorf("initial background-color = %v, want transparent zero value", cs.BackgroundColor)
+	}
+	// Unset margins/paddings are 0px (the Length zero value).
+	if cs.MarginBottom != (Length{}) || cs.PaddingLeft != (Length{}) {
+		t.Errorf("initial margin-bottom/padding-left = %v/%v, want zero 0px", cs.MarginBottom, cs.PaddingLeft)
+	}
 }
