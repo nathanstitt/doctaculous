d636775834213a3880e9e7d1b52aa27785260610
Nathan Stitt <nathan@stitt.org>
2026-06-23 20:29:51 -0500

    Add golang.org/x/net dependency for the HTML frontend

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/benoitkugler/textlayout v0.3.2
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef
 	golang.org/x/image v0.43.0
+	golang.org/x/net v0.43.0
 )
 
 require (
---
+++ pkg/html/html.go
@@ -0,0 +1,7 @@
+// Package html parses HTML documents and lowers them to the box model
+// for the reflow engine. It uses golang.org/x/net/html for HTML5 parsing.
+package html
+
+import (
+	_ "golang.org/x/net/html" // HTML5 parser; used by the frontend (stub until Task 2)
+)
